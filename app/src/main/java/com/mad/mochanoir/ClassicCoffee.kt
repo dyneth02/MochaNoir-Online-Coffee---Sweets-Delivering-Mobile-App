c5eb3203b910b91c153f2c1f82921d5e6cb051b2
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2026-02-25 00:28:34 +0530

    Add files via upload

---
+++ app/src/main/java/com/mad/mochanoir/ClassicCoffee.kt
@@ -0,0 +1,66 @@
+package com.mad.mochanoir
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.mad.mochanoir.databinding.FragmentClassicCoffeeBinding
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class ClassicCoffee : Fragment() {
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var _binding: FragmentClassicCoffeeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentClassicCoffeeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    fun getRedVImageView(): ImageView? {
+        return binding.chocoFrost
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.chocoFrost.setOnClickListener {
+            val dialog = CustomDialogFragment2()
+            dialog.show(parentFragmentManager, "customDialog2")
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ClassicCoffee().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
